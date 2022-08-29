8d03f75d46f58200755ec558c93c8b2b46138c5f
Ran Ribenzaft <ranrib@users.noreply.github.com>
2022-08-29 16:35:47 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# disney-plus-subtitles-changer
+
+a Chrome extenstion that changes the font for Disney Plus service.
---
+++ src/content_script.js
@@ -0,0 +1,10 @@
+
+function addStyle(styleString) {
+  const style = document.createElement('style');
+  style.textContent = styleString;
+  document.head.append(style);
+}
+
+if (document.location.host.includes(".disneyplus.")) {
+  addStyle('.shaka-text-container > span { font-family: Arial !important; font-size: 40px !important; background-color: rgba(0, 0, 0, 0.0) !important; -webkit-text-fill-color: white; -webkit-text-stroke-color: black; -webkit-text-stroke-width: 1.00px; }')
+}
