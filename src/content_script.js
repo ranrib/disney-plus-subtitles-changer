38a8a64d8745c27a959161022c4afa04184796ad
Ran Ribenzaft <ranrib@users.noreply.github.com>
2022-09-12 10:54:28 +0300

    Update content_script.js

--- src/content_script.js
+++ src/content_script.js
@@ -6,5 +6,5 @@ function addStyle(styleString) {
 }
 
 if (document.location.host.includes(".disneyplus.")) {
-  addStyle('.shaka-text-container > span { font-family: Arial !important; font-size: 40px !important; background-color: rgba(0, 0, 0, 0.0) !important; -webkit-text-fill-color: white; -webkit-text-stroke-color: black; -webkit-text-stroke-width: 1.00px; }')
+  addStyle('.shaka-text-container > span { font-family: Arial !important; font-size: 50px !important; background-color: rgba(0, 0, 0, 0.0) !important; -webkit-text-fill-color: white; -webkit-text-stroke-color: black; -webkit-text-stroke-width: 1.00px; width: 1000px !important; }')
 }
